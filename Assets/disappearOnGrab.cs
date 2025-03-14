bc338a19f49c43368c901dc6af413de33f7e1eeb
Justin Sato <justinsccs1@gmail.com>
2025-03-14 14:11:22 -0700

    latest code for disappear on grab

--- Assets/disappearOnGrab.cs
+++ Assets/disappearOnGrab.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class disappearOnGrab : MonoBehaviour
-{
-    private Collider objectCollider;
+{  
     private XRGrabInteractable grabInteractable;
-    // Start is called before the first frame update
     void Start()
     {
-        objectCollider = GetComponent <Collider>();
+        
         grabInteractable = GetComponent<XRGrabInteractable>();
-
+        grabInteractable.selectEntered.AddListener(OnGrab);
         
     }
     private void OnGrab(SelectEnterEventArgs args){
