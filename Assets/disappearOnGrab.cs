804b78080d2f39f5bfdc257f3cb33a86fd490ddf
Justin Sato <justinsccs1@gmail.com>
2025-03-20 11:52:46 -0700

    the goggles work now!
    the directional light dims and the goggles disappear on grab

--- Assets/disappearOnGrab.cs
+++ Assets/disappearOnGrab.cs
@@ -6,14 +6,15 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class disappearOnGrab : MonoBehaviour
 {  
     private XRGrabInteractable grabInteractable;
+    public Light directionalLight;
+
     void Start()
     {
-        
         grabInteractable = GetComponent<XRGrabInteractable>();
         grabInteractable.selectEntered.AddListener(OnGrab);
-        
     }
     private void OnGrab(SelectEnterEventArgs args){
         gameObject.SetActive(false);
+        directionalLight.intensity = 0.2f;
     }
 }
