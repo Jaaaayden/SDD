43c4b337e82f4327cda3902621e28243be4b1fc0
Nqchoz <joshpham15@gmail.com>
2025-05-01 15:49:04 -0700

    destroy marble
    marble will not spawn after destroyed, but it does destory ( i think)

--- Assets/Scripts/MarbleSpawner.cs
+++ Assets/Scripts/MarbleSpawner.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class MarbleSpawner : MonoBehaviour
 {
     public GameObject marblePrefab;
     public Transform spawnPoint;
     private GameObject marbleInstance;
 
+    public TMP_Text text;
+
     
     public void SpawnMarble(){
         if(marbleInstance == null){
             marbleInstance = Instantiate(marblePrefab, spawnPoint);
         }
+        else{
+            text.text = marbleInstance.name;
+        }
     } 
 
     public void DestroyMarble(){
         Destroy(marbleInstance);
+        marbleInstance = null;
     }
 }
