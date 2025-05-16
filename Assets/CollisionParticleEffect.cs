ad570152aaba86816c4c0d603e7baab82e4d4791
Justin Sato <justinsccs1@gmail.com>
2025-05-16 11:10:50 -0700

    broken but with some addons

---
+++ Assets/CollisionParticleEffect.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
+
+public class CollisionParticleEffect : MonoBehaviour
+{
+    public ParticleSystem particleEffect;
+
+    private void Start()
+    {
+        if (particleEffect != null)
+            particleEffect.Stop();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the other object has an XRGrabInteractable
+        XRGrabInteractable otherGrab = collision.gameObject.GetComponent<XRGrabInteractable>();
+        XRGrabInteractable thisGrab = GetComponent<XRGrabInteractable>();
+
+        if (otherGrab != null && thisGrab != null)
+        {
+            // Start delayed particle effect
+            Vector3 contactPoint = collision.contacts[0].point;
+            StartCoroutine(PlayEffectAfterDelay(contactPoint, 4f));
+        }
+    }
+
+    private IEnumerator PlayEffectAfterDelay(Vector3 position, float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+
+        if (particleEffect != null)
+        {
+            particleEffect.transform.position = position;
+            particleEffect.Play();
+        }
+    }
+}
